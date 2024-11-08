98a82e51df5e10884dc061dc5489386e47b3e63d
Elizaveta Platonova <EPlatonova@nota.tech>
2024-11-08 13:14:45 +0500

    поменяла api

--- src/api/dto.ts
+++ src/api/dto.ts
@@ -1,21 +1,18 @@
 export type MovieDTO = {
-  id: number,
-  title: string,
-  year: number,
-  genre: string[],
-  rating: number,
-  director: string,
-  actors: string[],
-  plot: string,
-  poster: string,
-  trailer: string,
-  runtime: number,
-  awards: string,
-  country: string,
-  language: string,
-  boxOffice: string,
-  production: string,
-  website: string
+  imdbID: string,
+  Title: string,
+  Year: number,
+  Type: string,
+  Poster: string,
+
+  imdbRating: number,
+  Genre: string,
+  Country: string,
+  Plot: string,
 }
 
-export type MoviesDTO = MovieDTO[];
+export type MoviesDTO = {Search: {imdbID: string,
+  Title: string,
+  Year: number,
+  Type: string,
+  Poster: string}[]};
--- src/api/mappers.ts
+++ src/api/mappers.ts
@@ -2,9 +2,28 @@ import { Movie } from "../entities/movie";
 import { MovieDTO, MoviesDTO } from "./dto";
 
 export function mapMovieDTOToMovie(dto: MovieDTO): Movie {
-  return dto as Movie;
+  return ({
+    id: dto.imdbID,
+    title: dto.Title,
+    year: dto.Year,
+    poster: dto.Poster,
+    rating: dto.imdbRating,
+    genre: [dto.Genre],
+    country: dto.Country,
+    plot: dto.Plot,
+  })
+  
 }
 
 export function mapMoviesDTOToMovies(dto: MoviesDTO): Movie[] {
-  return dto.map(mapMovieDTOToMovie);
+  return dto.Search.map((dto) => ({
+    id: dto.imdbID,
+    title: dto.Title,
+    year: dto.Year,
+    poster: dto.Poster,
+    rating: 10,
+    genre: [""],
+    country: "",
+    plot: "",
+  }));
 }
